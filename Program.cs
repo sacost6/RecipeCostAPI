d2c9c36eeaa9a495a393732b58efe1204500caf3
Simon <sacost6@uic.edu>
2026-03-04 23:18:26 -0600

    Modified Program.cs to add support for blazor

--- Program.cs
+++ Program.cs
@@ -20,7 +20,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:3000")
+                          policy.WithOrigins("http://localhost:3000",
+                                             "http://localhost:5168",
+                                             "http://localhost:5168")
+                            
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
